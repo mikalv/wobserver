6431c11d9e17a153eeb5d66ea05ffb5f17547120
Mikal Villa <mikalv@mikalv.net>
2020-01-09 11:30:53 +0100

    Umbrella mix config

--- mix.exs
+++ mix.exs
@@ -5,11 +5,15 @@ defmodule Wobserver.Mixfile do
     [
       app: :wobserver,
       version: "0.1.8",
-      elixir: "~> 1.4",
       description: "Web based metrics, monitoring, and observer.",
       package: package(),
-      build_embedded: Mix.env == :prod,
-      start_permanent: Mix.env == :prod,
+      build_path: "../../_build",
+      config_path: "../../config/config.exs",
+      deps_path: "../../deps",
+      lockfile: "../../mix.lock",
+      elixir: "~> 1.9",
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
       deps: deps(),
       # Testing
       test_coverage: [tool: ExCoveralls],
@@ -64,16 +68,16 @@ defmodule Wobserver.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      {:cowboy, "~> 1.1"},
-      {:credo, "~> 0.7", only: [:dev, :test]},
+      {:cowboy, "~> 2.5"},
+      #{:credo, "~> 1.1.5", only: [:dev, :test]},
       {:dialyxir, "~> 0.5", only: [:dev], runtime: false},
       {:ex_doc, "~> 0.15", only: :dev},
       {:excoveralls, "~> 0.6", only: :test},
-      {:httpoison, "~> 0.11 or ~> 0.12"},
-      {:inch_ex, "~> 0.5", only: [:dev, :test]},
-      {:meck, "~> 0.8.4", only: :test},
+      {:httpoison, "~> 1.6"},
+      {:inch_ex, "~> 2.0", only: [:dev, :test]},
+      {:meck, "~> 0.8.13", only: :test},
       {:plug, "~> 1.3 or ~> 1.4"},
-      {:poison, "~> 2.0 or ~> 3.1"},
+      {:poison, "~> 4.0"},
       {:websocket_client, "~> 1.2"},
     ]
   end
